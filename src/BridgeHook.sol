9fc2479773c4e1b3c6223b425f3b54aae4deca2b
john doe <you@example.com>
2024-05-01 11:27:50 +0000

    test

--- src/BridgeHook.sol
+++ src/BridgeHook.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import {BaseHook} from "v4-periphery/BaseHook.sol";
@@ -16,137 +16,193 @@ import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 
 contract HashedTimelockHook is BaseHook, ReentrancyGuard, Ownable {
+    using CurrencyLibrary for Currency;
+    using BalanceDeltaLibrary for BalanceDelta;
+
+    mapping (bytes32 => LockContract) public contracts;
+    int24 feeProportional;
+
+    event HTLCERC20New(
+        address indexed sender,
+        address indexed receiver,
+        address tokenContract,
+        uint256 amount,
+        bytes32 hashlock,
+        uint256 timelock
+    );
+    event HTLCERC20Withdraw(bytes32 indexed hashlock);
+    event HTLCERC20Refund(bytes32 indexed hashlock);
+
+    struct LockContract {
+        address sender;
+        address receiver;
+        address tokenContract;
+        uint256 amount;
+        // locked UNTIL this time. Unit depends on consensus algorithm.
+        // PoA, PoA and IBFT all use seconds. But Quorum Raft uses nano-seconds
+        uint256 timelock;
+        bool withdrawn;
+        bool refunded;
+        bytes32 preimage;
+    }
+
+    modifier tokensTransferable(address _token, address _sender, uint256 _amount) {
+        require(_amount > 0, "token amount must be > 0");
+        require(
+            ERC20(_token).allowance(_sender, address(this)) >= _amount,
+            "token allowance must be >= amount"
+        );
+        _;
+    }
+    modifier futureTimelock(uint256 _time) {
+        // only requirement is the timelock time is after the last blocktime (now).
+        // probably want something a bit further in the future then this.
+        // but this is still a useful sanity check:
+        require(_time > block.timestamp, "timelock time must be in the future");
+        _;
+    }
+    modifier contractExists(bytes32 _hashlock) {
+        require(haveContract(_hashlock), "contract does not exist");
+        _;
+    }
+    modifier hashlockMatches(bytes32 _hashlock, bytes32 _x) {
+        require(
+            _hashlock == sha256(abi.encodePacked(_x)),
+            "hashlock hash does not match"
+        );
+        _;
+    }
+    modifier withdrawable(bytes32 _hashlock) {
+        require(contracts[_hashlock].receiver == msg.sender, "withdrawable: not receiver");
+        require(contracts[_hashlock].withdrawn == false, "withdrawable: already withdrawn");
+        // if we want to disallow claim to be made after the timeout, uncomment the following line
+        // require(contracts[_hashlock].timelock > now, "withdrawable: timelock time must be in the future");
+        _;
+    }
+    modifier refundable(bytes32 _hashlock) {
+        require(contracts[_hashlock].sender == msg.sender, "refundable: not sender");
+        require(contracts[_hashlock].refunded == false, "refundable: already refunded");
+        require(contracts[_hashlock].withdrawn == false, "refundable: already withdrawn");
+        require(contracts[_hashlock].timelock <= block.timestamp, "refundable: timelock not yet passed");
+        _;
+    }
+
+    function newContract(
+        address _receiver,
+        bytes32 _hashlock,
+        uint256 _timelock,
+        address _tokenContract,
+        uint256 _amount
+    )
+    external
+    tokensTransferable(_tokenContract, msg.sender, _amount)
+    futureTimelock(_timelock)
+    returns (bytes32 hashlock)
+    {
+        if (haveContract(_hashlock))
+            revert("Contract already exists");
+
+        if (!ERC20(_tokenContract).transferFrom(msg.sender, address(this), _amount))
+            revert("transferFrom sender to this failed");
+
+        contracts[_hashlock] = LockContract(
+            msg.sender,
+            _receiver,
+            _tokenContract,
+            _amount,
+            _timelock,
+            false,
+            false,
+            0x0
+        );
+
+        emit HTLCERC20New(
+            msg.sender,
+            _receiver,
+            _tokenContract,
+            _amount,
+            _hashlock,
+            _timelock
+        );
+
+        return _hashlock;
+    }
+
+    function withdraw(bytes32 _hashlock, bytes32 _preimage)
+    external
+    contractExists(_hashlock)
+    hashlockMatches(_hashlock, _preimage)
+    withdrawable(_hashlock)
+    returns (bool)
+    {
+        LockContract storage c = contracts[_hashlock];
+        c.preimage = _preimage;
+        c.withdrawn = true;
+        ERC20(c.tokenContract).transfer(c.receiver, c.amount);
+        emit HTLCERC20Withdraw(_hashlock);
+        return true;
+    }
+
+    function refund(bytes32 _hashlock)
+    external
+    contractExists(_hashlock)
+    refundable(_hashlock)
+    returns (bool)
+    {
+        LockContract storage c = contracts[_hashlock];
+        c.refunded = true;
+        ERC20(c.tokenContract).transfer(c.sender, c.amount);
+        emit HTLCERC20Refund(_hashlock);
+        return true;
+    }
+
+    function getContract(bytes32 _hashlock)
+    public
+    view
+    returns (
+        bytes32 hashlock,
+        address sender,
+        address receiver,
+        address tokenContract,
+        uint256 amount,
+        uint256 timelock,
+        bool withdrawn,
+        bool refunded,
+        bytes32 preimage
+    )
+    {
+        if (haveContract(_hashlock) == false)
+            return (0, address(0), address(0), address(0), 0, 0, false, false, 0);
+        LockContract storage c = contracts[_hashlock];
+        return (
+        _hashlock,
+        c.sender,
+        c.receiver,
+        c.tokenContract,
+        c.amount,
+        c.timelock,
+        c.withdrawn,
+        c.refunded,
+        c.preimage
+        );
+    }
+
+
+    function haveContract(bytes32 _hashlock)
+    internal
+    view
+    returns (bool exists)
+    {
+        exists = (contracts[_hashlock].sender != address(0));
+    }
+
     constructor(
         IPoolManager _manager,
         address initialOwner,
         int24 initialFeeProportional
     ) BaseHook(_manager) Ownable(initialOwner) {
         feeProportional = initialFeeProportional;
-        resetContractState();
     }
-
-    event HTLCERC20New(
-        bytes32 indexed contractId,
-        bool incoming,
-        address counterparty,
-        address tokenContract,
-        uint256 amount,
-        uint256 timelock
-    );
-    event HTLCERC20Withdraw(bytes32 indexed contractId);
-    event HTLCERC20Refund(bytes32 indexed contractId);
-
-    struct LockContract {
-        bool incoming;
-        address counterparty;
-        address tokenContract;
-        uint256 amount;
-        uint256 timelock; // locked UNTIL this time. Unit depends on consensus algorithm. PoA, PoA and IBFT all use seconds. But Quorum Raft uses nano-seconds
-        bytes32 preimage;
-    }
-
-    modifier contractExists(bytes32 _contractId) {
-        require(haveContract(_contractId), "contractId does not exist");
-        _;
-    }
-
-    modifier futureTimelock(uint256 _time) {
-        require(_time > block.timestamp, "timelock time must be in the future");
-        _;
-    }
-
-    modifier pastTimelock() {
-        require(counterparty != address(0), "Not in settlement state");
-        require(timelock <= block.timestamp, "Timelock not yet passed");
-        _;
-    }
-
-    modifier hashlockMatches(bytes32 _hashlock, bytes32 _x) {
-        require(_hashlock == sha256(abi.encodePacked(_x)), "hashlock hash does not match");
-        _;
-    }
-
-    modifier transferable() {
-        require(counterparty == address(0), "Contract is in settlement state");
-        _;
-    }
-
-    modifier locked() {
-        require(counterparty != address(0), "Not in settlement state");
-        _;
-    }
-
-    function newContract(
-        address _counterparty,
-        bool _incoming,
-        bytes32 _hashlock,
-        uint256 _timelock,
-        address _tokenContract,
-        uint256 _amount
-    ) external futureTimelock(_timelock) returns (bool) {
-        if (_incoming) {
-            require(_amount > 0, "Token amount must be > 0");
-            require(IERC20(_tokenContract).allowance(_counterparty, address(this)) >= _amount, "Token allowance must be >= amount");
-            IERC20(_tokenContract).transferFrom(_counterparty, address(this), _amount);
-        }
-
-        incoming = _incoming;
-        counterparty = _counterparty;
-        tokenContract = _tokenContract;
-        amount = _amount;
-        timelock = _timelock;
-        hashlock = _hashlock;
-
-        emit HTLCERC20New(_counterparty, _tokenContract, _amount, _hashlock, _timelock);
-
-        return true;
-    }
-
-    function settle(bytes32 _preimage) external nonReentrant hashlockMatches(hashlock, _preimage) locked returns (bool) {
-        if (incoming) {
-            emit HTLCERC20Settle(hashlock, tokenContract, amount);
-        } else {
-            IERC20(tokenContract).transfer(counterparty, amount);
-            emit HTLCERC20Settle(hashlock, tokenContract, amount);
-        }
-        resetContractState();
-        return true;
-    }
-
-    function refund() external nonReentrant locked pastTimelock returns (bool) {
-        if (!incoming) IERC20(tokenContract).transfer(counterparty, amount);
-        emit HTLCERC20Refund(hashlock, tokenContract, amount);
-        resetContractState();
-        return true;
-    }
-
-    function transfer(address _tokenContract, uint256 _amount) external onlyOwner locked returns (bool) {
-        IERC20(_tokenContract).transfer(counterparty, _amount);
-        return true;
-    }
-
-    function resetContractState() internal {
-        counterparty = address(0);
-        tokenContract = address(0);
-        amount = 0;
-        timelock = 0;
-        hashlock = 0x0;
-    }
-
-    mapping(bytes32 => LockContract) contracts;
-    int24 feeProportional;
-
-    // Use CurrencyLibrary and BalanceDeltaLibrary
-    // to add some helper functions over the Currency and BalanceDelta
-    // data types
-    using CurrencyLibrary for Currency;
-    using BalanceDeltaLibrary for BalanceDelta;
-
-    // Keeping track of user => referrer
-    mapping(address => address) public referredBy;
-
-    // Amount of points someone gets for referring someone else
-    uint256 public constant POINTS_FOR_REFERRAL = 500 * 10 ** 18;
 
     // Set up hook permissions to return `true`
     // for the two hook functions we are using
